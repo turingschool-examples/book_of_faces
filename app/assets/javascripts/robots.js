b22a3e2ac491e11459a318355b840954df6d1ee9
Steve Kinney <hello@stevekinney.net>
2015-07-13 15:48:39 -0600

    Show all robots if the dropdown is blank

--- app/assets/javascripts/robots.js
+++ app/assets/javascripts/robots.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
   $('#robot_filter_department').on('change', function () {
     var currentDepartment = this.value;
+    if (!currentDepartment) { return $robots.show(); }
     $robots.each(function (index, robot) {
       $robot = $(robot);
       if ($robot.data('department') === currentDepartment) {
